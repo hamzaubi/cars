0f14d06c0e7922c037e47fdc329972fbf8056257
Laravel Daily <info@laraveldaily.com>
2024-01-06 13:41:57 +0000

    QuickAdminPanel automatic commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,6 +20,10 @@
                             <h3>{!! $chart1->options['chart_title'] !!}</h3>
                             {!! $chart1->renderHtml() !!}
                         </div>
+                        <div class="{{ $chart2->options['column_class'] }}">
+                            <h3>{!! $chart2->options['chart_title'] !!}</h3>
+                            {!! $chart2->renderHtml() !!}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -29,5 +33,5 @@
 @endsection
 @section('scripts')
 @parent
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>{!! $chart1->renderJs() !!}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>{!! $chart1->renderJs() !!}{!! $chart2->renderJs() !!}
 @endsection
